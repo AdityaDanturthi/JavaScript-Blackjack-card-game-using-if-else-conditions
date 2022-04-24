08b23d5491a72eec0750499f254a49a89148b4fc
Aditya Danturthi <aditya2096@gmail.com>
2022-04-23 21:40:36 -0230

    Add files via upload

--- Blackjack card game using if else conditions.js
+++ Blackjack card game using if else conditions.js
@@ -1,14 +1,27 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
 
 let sum = firstCard + secondCard
 
-if(sum < 21) {
-    console.log("Do you want to draw another card?")
+// if(sum < 21) {
+//     console.log("Do you want to draw another card?")
+// }
+// else if (sum === 21) {
+//     console.log("Congratulations! You have a blackjack! You won!")
+// }
+// else {
+//     console.log("Sorry! You are out of the game.")
+// }
+
+// if less than or equal to 20 -> "Do you want to draw a new card?"
+// else if exactly 21 -> "Wohoo! You've got Blackjack"
+// else -> "You're out of the game!"
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?")
 }
 else if (sum === 21) {
-    console.log("Congratulations! You have a blackjack! You won!")
+    console.log("Wohoo! You've got Blackjack")
 }
 else {
-    console.log("Sorry! You are out of the game.")
+    console.log("You're out of the game!")
 }
